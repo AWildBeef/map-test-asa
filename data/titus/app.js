061bde64b4e0782efec4bd8bdf337b151d6cd022
AWildBeef <117673158+AWildBeef@users.noreply.github.com>
2026-02-03 10:17:12 -0700

    Update app.js

--- data/titus/app.js
+++ data/titus/app.js
@@ -1165,7 +1165,7 @@ function ensurePanels() {
     infoPanel = createFloatingPanel({
       id: "dinoInfoPanel",
       title: "Dino Info",
-      defaultPos: { right: 218, top: 2 },
+      defaultPos: { left: 2, top: 2 },
       collapsedByDefault: true
     });
     renderInfoPanelBodyEmpty();
