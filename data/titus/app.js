3a305909273b7a4bae9897cba3c7f4e06c03a375
AWildBeef <117673158+AWildBeef@users.noreply.github.com>
2026-02-06 10:23:32 -0700

    Update app.js

--- data/titus/app.js
+++ data/titus/app.js
@@ -81,7 +81,7 @@ function normalizePoiType(type) {
 // DRAWING TUNING
 // ============================================================
 const BOX_TO_POINT_AREA_THRESHOLD = 18_000;
-const BOX_TO_POINT_MIN_DIM = 100;
+const BOX_TO_POINT_MIN_DIM = 10;
 
 // ============================================================
 // MAPS
