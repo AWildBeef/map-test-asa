ab8168ff18d63744c9fcdb487b5b98358bd91445
AWildBeef <117673158+AWildBeef@users.noreply.github.com>
2026-03-27 10:31:26 -0600

    i

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,7 @@
 (function () {
+  const isEmbedded = window.self !== window.top;
   const params = new URLSearchParams(window.location.search);
-  const isDiscordLike = true;
+  const isDiscordLike = isEmbedded || params.get("discord") === "1";
 
   window.ASA_RUNTIME = {
     isDiscordActivity: isDiscordLike,
@@ -12,10 +13,18 @@
     }
   };
 
-  document.title = "ASA Spawn Activity";
-  document.body.classList.add("discord-activity");
+  document.title = isDiscordLike ? "ASA Spawn Activity" : "ASA Spawn Maps";
+
+  if (isDiscordLike) {
+    document.body.classList.add("discord-activity");
+  }
+
+  console.log("ENTRY LOADED");
+  console.log("isEmbedded:", isEmbedded);
+  console.log("isDiscordLike:", isDiscordLike);
+  console.log("body classes:", document.body.className);
 
   const script = document.createElement("script");
-  script.src = "js/app.js?v=force1";
+  script.src = "js/app.js?v=4";
   document.head.appendChild(script);
 })();
